6123c8ba580e55ddd64c17df982a16310ce45373
John Reemar Dela Rosa <jrddelarosa@gmail.com>
2022-11-25 14:33:09 +0800

    added new animation

--- main.js
+++ main.js
@@ -1,128 +1,141 @@
-import './style.css';
+'use strict';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-const scene = new THREE.Scene();
+let scene,
+    camera,
+    renderer,
+    controls;
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let particles,
+    saturn;
 
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
-});
+let width = window.innerWidth,
+    height = window.innerHeight;
 
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
-camera.position.setX(-3);
+const colors = [0x37BE95, 0xF3F3F3, 0x6549C0];
 
-renderer.render(scene, camera);
+init();
+animate();
 
-// Torus
+function init() {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+  camera.lookAt(scene.position);
+  camera.position.z = 500;
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
-const torus = new THREE.Mesh(geometry, material);
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(width, height);
+  renderer.setClearColor(0x0E2255);
+  renderer.shadowMap.enabled = true;
+ 
+  // controls = new THREE.OrbitControls(camera, renderer.domElement);
+  
+  const ambientLight = new THREE.AmbientLight();
+  scene.add(ambientLight);
 
-scene.add(torus);
+  const light = new THREE.DirectionalLight();
+  light.position.set(200, 100, 200);
+  light.castShadow = true;
+  light.shadow.camera.left = -100;
+  light.shadow.camera.right = 100;
+  light.shadow.camera.top = 100;
+  light.shadow.camera.bottom = -100;
+  scene.add(light);
+  
+  drawParticles();
+  drawSaturn();
 
-// Lights
+  document.getElementById('world').appendChild(renderer.domElement);
 
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(5, 5, 5);
-
-const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(pointLight, ambientLight);
-
-// Helpers
-
-const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper, gridHelper)
-
-const controls = new OrbitControls(camera, renderer.domElement);
-
-function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
-  const star = new THREE.Mesh(geometry, material);
-
-  const [x, y, z] = Array(3)
-    .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(100));
-
-  star.position.set(x, y, z);
-  scene.add(star);
+  window.addEventListener('resize', onResize);
 }
 
-Array(200).fill().forEach(addStar);
-
-// Background
-
-const spaceTexture = new THREE.TextureLoader().load('space.jpg');
-scene.background = spaceTexture;
-
-// Avatar
-
-const jeffTexture = new THREE.TextureLoader().load('john-qwik.png');
-
-const jeff = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: jeffTexture }));
-
-scene.add(jeff);
-
-// Moon
-
-const moonTexture = new THREE.TextureLoader().load('moon.jpg');
-const normalTexture = new THREE.TextureLoader().load('normal.jpg');
-
-const moon = new THREE.Mesh(
-  new THREE.SphereGeometry(3, 32, 32),
-  new THREE.MeshStandardMaterial({
-    map: moonTexture,
-    normalMap: normalTexture,
-  })
-);
-
-scene.add(moon);
-
-moon.position.z = 30;
-moon.position.setX(-10);
-
-jeff.position.z = -5;
-jeff.position.x = 2;
-
-// Scroll Animation
-
-function moveCamera() {
-  const t = document.body.getBoundingClientRect().top;
-  moon.rotation.x += 0.05;
-  moon.rotation.y += 0.075;
-  moon.rotation.z += 0.05;
-
-  jeff.rotation.y += 0.01;
-  jeff.rotation.z += 0.01;
-
-  camera.position.z = t * -0.01;
-  camera.position.x = t * -0.0002;
-  camera.rotation.y = t * -0.0002;
+function onResize() {
+  width = window.innerWidth;
+  height = window.innerHeight;
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(width, height);
 }
-
-document.body.onscroll = moveCamera;
-moveCamera();
-
-// Animation Loop
 
 function animate() {
   requestAnimationFrame(animate);
 
-  torus.rotation.x += 0.02;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.1;
+  render();
+}
 
-  moon.rotation.x += 0.005;
-
-  // controls.update();
-
+function render() {
+  particles.rotation.x += 0.001;
+  particles.rotation.y -= 0.004;
+  saturn.rotation.y += 0.003;
   renderer.render(scene, camera);
 }
 
-animate();
+function drawParticles() {
+  particles = new THREE.Group();
+  scene.add(particles);
+  const geometry = new THREE.TetrahedronGeometry(5, 0);
+  
+  for (let i = 0; i < 500; i ++) {
+    const material = new THREE.MeshPhongMaterial({
+      color: colors[Math.floor(Math.random() * colors.length)],
+      size: Math.random(100),
+      shading: THREE.FlatShading
+    });
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set((Math.random() - 0.5) * 1000,
+                      (Math.random() - 0.5) * 1000,
+                      (Math.random() - 0.5) * 1000);
+    mesh.updateMatrix();
+    mesh.matrixAutoUpdate = false;
+    particles.add(mesh);
+  }
+}
+
+function drawSaturn() {
+  saturn = new THREE.Group();
+  saturn.rotation.set(0.4, 0.3, 0);
+  scene.add(saturn);
+  
+  const planetGeometry = new THREE.IcosahedronGeometry(100, 1);
+  
+  const planetMaterial = new THREE.MeshPhongMaterial({
+    color: 0x37BE95,
+    shading: THREE.FlatShading
+    });
+  const planet = new THREE.Mesh(planetGeometry, planetMaterial);
+  
+  planet.castShadow = true;
+  planet.receiveShadow = true;
+  planet.position.set(0, 40, 0);
+  saturn.add(planet);
+  
+  const ringGeometry = new THREE.TorusGeometry(140, 12, 6, 105);
+  const ringMeterial = new THREE.MeshStandardMaterial({
+    color: 0x6549C0,
+    shading: THREE.FlatShading
+  });
+  const ring = new THREE.Mesh(ringGeometry, ringMeterial);
+  ring.position.set(0, 40, 0)
+  ring.rotateX(90);
+  saturn.rotation.set(1.1, 1.3, .3);
+  ring.castShadow = true;
+  ring.receiveShadow = true;
+  
+  const ringGeometry2 = new THREE.TorusGeometry(140, 1, 6, 105);
+  const ringMeterial2 = new THREE.MeshStandardMaterial({
+    color: 0x6549C0,
+    shading: THREE.FlatShading
+  });
+  
+  const ring2 = new THREE.Mesh(ringGeometry2, ringMeterial2);
+  ring2.position.set(0, 40, 0)
+  ring2.rotateX(120);
+  
+  saturn.add(ring);
+  saturn.add(ring2);
+  
+}
+
